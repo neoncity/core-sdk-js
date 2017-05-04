38f490201d7eba68318efc32ef95972f7b7688c3
Horia Coman <horia141@gmail.com>
2017-05-04 11:02:21 +0300

    Damn out-of-order issues

--- src/index.ts
+++ src/index.ts
@@ -199,32 +199,6 @@ export class PrivateCause extends Cause {
 }
 
 
-export enum CauseEventType {
-    Unknown = 0,
-    Created = 1,
-    Updated = 2,
-    Succeeded = 3,
-    Removed = 4
-}
-
-
-export class CauseEvent {
-    @MarshalWith(r.IdMarshaller)
-    id: number;
-
-    @MarshalWith(MarshalEnum(CauseEventType))
-    type: CauseEventType;
-
-    @MarshalWith(r.TimeMarshaller)
-    timestamp: Date;
-
-    @MarshalWith(OptionalOf(OneOf2(
-        MarshalFrom(CreateCauseRequest),
-        MarshalFrom(UpdateCauseRequest))))
-    data: CreateCauseRequest|UpdateCauseRequest|null;
-}
-
-
 export class DonationForCause {
     @MarshalWith(r.IdMarshaller)
     id: number;
@@ -255,27 +229,6 @@ export class DonationForUser {
 }
 
 
-export enum DonationEventType {
-    Unknown = 0,
-    Created = 1
-}
-
-
-export class DonationEvent {
-    @MarshalWith(r.IdMarshaller)
-    id: number;
-
-    @MarshalWith(MarshalEnum(DonationEventType))
-    type: DonationEventType;
-
-    @MarshalWith(r.TimeMarshaller)
-    timestamp: Date;
-
-    @MarshalWith(OptionalOf(MarshalFrom(CreateDonationRequest)))
-    data: CreateDonationRequest|null;
-}
-
-
 export class ShareForCause {
     @MarshalWith(r.IdMarshaller)
     id: number;
@@ -303,27 +256,6 @@ export class ShareForUser {
 
     @MarshalWith(MarshalFrom(PublicCause))
     forCause: PublicCause;
-}
-
-
-export enum ShareEventType {
-    Unknown = 0,
-    Created = 1
-}
-
-
-export class ShareEvent {
-    @MarshalWith(r.IdMarshaller)
-    id: number;
-
-    @MarshalWith(MarshalEnum(ShareEventType))
-    type: ShareEventType;
-
-    @MarshalWith(r.TimeMarshaller)
-    timestamp: Date;
-
-    @MarshalWith(OptionalOf(MarshalFrom(CreateShareRequest)))
-    data: CreateShareRequest|null;
 }
 
 
@@ -423,6 +355,74 @@ export class UpdateCauseRequest {
 export class ActionsOverviewResponse {
     @MarshalWith(MarshalFrom(UserActionsOverview))
     actionsOverview: UserActionsOverview;
+}
+
+
+export enum CauseEventType {
+    Unknown = 0,
+    Created = 1,
+    Updated = 2,
+    Succeeded = 3,
+    Removed = 4
+}
+
+
+export class CauseEvent {
+    @MarshalWith(r.IdMarshaller)
+    id: number;
+
+    @MarshalWith(MarshalEnum(CauseEventType))
+    type: CauseEventType;
+
+    @MarshalWith(r.TimeMarshaller)
+    timestamp: Date;
+
+    @MarshalWith(OptionalOf(OneOf2(
+        MarshalFrom(CreateCauseRequest),
+        MarshalFrom(UpdateCauseRequest))))
+    data: CreateCauseRequest|UpdateCauseRequest|null;
+}
+
+
+export enum DonationEventType {
+    Unknown = 0,
+    Created = 1
+}
+
+
+export class DonationEvent {
+    @MarshalWith(r.IdMarshaller)
+    id: number;
+
+    @MarshalWith(MarshalEnum(DonationEventType))
+    type: DonationEventType;
+
+    @MarshalWith(r.TimeMarshaller)
+    timestamp: Date;
+
+    @MarshalWith(OptionalOf(MarshalFrom(CreateDonationRequest)))
+    data: CreateDonationRequest|null;
+}
+
+
+export enum ShareEventType {
+    Unknown = 0,
+    Created = 1
+}
+
+
+export class ShareEvent {
+    @MarshalWith(r.IdMarshaller)
+    id: number;
+
+    @MarshalWith(MarshalEnum(ShareEventType))
+    type: ShareEventType;
+
+    @MarshalWith(r.TimeMarshaller)
+    timestamp: Date;
+
+    @MarshalWith(OptionalOf(MarshalFrom(CreateShareRequest)))
+    data: CreateShareRequest|null;
 }
 
 
